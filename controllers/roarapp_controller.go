ceab67f0bfb3478ffa29421d60ea7dc15bc49d41
Marco Yeung <myeung@redhat.com>
2021-05-03 11:18:58 -0400

    lab3

--- controllers/roarapp_controller.go
+++ controllers/roarapp_controller.go
@@ -18,6 +18,17 @@ package controllers
 
 import (
 	"context"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"reflect"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strconv"
+	"strings"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -34,9 +45,15 @@ type RoarAppReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+var nextPort = 0
+
 //+kubebuilder:rbac:groups=roarapp.roarapp.com,resources=roarapps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=roarapp.roarapp.com,resources=roarapps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=roarapp.roarapp.com,resources=roarapps/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;delete
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -52,6 +69,122 @@ func (r *RoarAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 	// your logic here
 
+	log := r.Log.WithValues("", req.NamespacedName)
+	log.Info("Reconciling instance")
+
+	instance := &roarappv1alpha1.RoarApp{}
+	err := r.Client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	// List all pods owned by this roarapp instance
+	podList := &corev1.PodList{}
+	lbs := map[string]string{
+		"app":     instance.Name,
+		"version": "v0.1",
+	}
+	labelSelector := labels.SelectorFromSet(lbs)
+	listOps := &client.ListOptions{Namespace: req.Namespace, LabelSelector: labelSelector}
+	if err = r.Client.List(context.TODO(), podList, listOps); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Count the pods that are pending or running as available
+	var available []corev1.Pod
+	for _, pod := range podList.Items {
+		if pod.ObjectMeta.DeletionTimestamp != nil {
+			continue
+		}
+		if pod.Status.Phase == corev1.PodRunning || pod.Status.Phase == corev1.PodPending {
+			available = append(available, pod)
+		}
+	}
+	availableNames := []string{}
+	for _, pod := range available {
+		availableNames = append(availableNames, pod.ObjectMeta.Name)
+	}
+
+	// Update the status if necessary
+	status := roarappv1alpha1.RoarAppStatus{
+		PodNames: availableNames,
+	}
+	if !reflect.DeepEqual(instance.Status, status) {
+		instance.Status = status
+		err = r.Client.Status().Update(context.TODO(), instance)
+		if err != nil {
+			log.Error(err, "Failed to update instance status")
+			return ctrl.Result{}, err
+		}
+	}
+
+	numAvailable := int32(len(available))
+	if numAvailable > instance.Spec.Replicas {
+		log.Info("Scaling down pods", "Currently available", numAvailable, "Required replicas", instance.Spec.Replicas)
+		diff := numAvailable - instance.Spec.Replicas
+		dpods := available[:diff]
+		for _, dpod := range dpods {
+			err = r.Client.Delete(context.TODO(), &dpod)
+			if err != nil {
+				log.Error(err, "Failed to delete pod", "pod.name", dpod.Name)
+				return ctrl.Result{}, err
+			}
+			log.Info("Scaling down corresponding service", "Pod", numAvailable, "Service", instance.Spec.Replicas)
+			strPort := dpod.Name[strings.LastIndex(dpod.Name, "-")+1:]
+			sName := instance.Name + "-service-" + strPort
+			//found := &appsv1.Deployment{}
+			s := &corev1.Service{}
+			err := r.Client.Get(context.TODO(), types.NamespacedName{
+				Name:      sName,
+				Namespace: req.Namespace,
+			}, s)
+			err = r.Client.Delete(context.TODO(), s)
+			if err != nil {
+				if errors.IsNotFound(err) {
+					// Return and don't requeue
+					return ctrl.Result{}, nil
+				}
+				// Error reading the object - requeue the request.
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	if numAvailable < instance.Spec.Replicas {
+		log.Info("Scaling up pods", "Currently available", numAvailable, "Required replicas", instance.Spec.Replicas)
+		// Define a new Pod object
+		pod := newPodForCR(instance)
+		// Set instance instance as the owner and controller
+		if err := controllerutil.SetControllerReference(instance, pod, r.Scheme); err != nil {
+			return reconcile.Result{}, err
+		}
+		err = r.Client.Create(context.TODO(), pod)
+		if err != nil {
+			log.Error(err, "Failed to create pod", "pod.name", pod.Name)
+			return ctrl.Result{}, err
+		}
+		// Define a new Service object
+		svc := newServiceForPod(instance)
+		// Set instance instance as the owner and controller
+		if err := controllerutil.SetControllerReference(instance, svc, r.Scheme); err != nil {
+			return reconcile.Result{}, err
+		}
+		err = r.Client.Create(context.TODO(), svc)
+		if err != nil {
+			log.Error(err, "Failed to create service", "svc.name", svc.Name)
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+
 	return ctrl.Result{}, nil
 }
 
@@ -60,4 +193,67 @@ func (r *RoarAppReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&roarappv1alpha1.RoarApp{}).
 		Complete(r)
+}
+
+func newServiceForPod(cr *roarappv1alpha1.RoarApp) *corev1.Service {
+
+	strPort := strconv.Itoa(nextPort)
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-service-" + strPort,
+			Namespace: cr.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: []corev1.ServicePort{{
+				Protocol:   corev1.ProtocolTCP,
+				Port:       8089,
+				TargetPort: intstr.FromInt(8080),
+				NodePort:   int32(nextPort),
+			}},
+			Type: corev1.ServiceTypeNodePort,
+		},
+	}
+}
+
+// newPodForCR returns a instance pod with the same name/namespace as the cr
+func newPodForCR(cr *roarappv1alpha1.RoarApp) *corev1.Pod {
+	if nextPort == 0 {
+		nextPort = 32000
+	} else {
+		nextPort++
+	}
+	strPort := strconv.Itoa(nextPort)
+	labels := map[string]string{
+		"app":      cr.Name,
+		"version":  "v0.1",
+		"nodePort": strPort,
+	}
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-pod-" + strPort,
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "roar-web",
+					Image:   cr.Spec.WebImage,
+					Command: []string{"catalina.sh", "run"},
+				},
+				{
+					Name:    "roar-db",
+					Image:   cr.Spec.DbImage,
+					Env:     []corev1.EnvVar{{Name: "MYSQL_USER", Value: "admin"}, {Name: "MYSQL_PASSWORD", Value: "admin"}, {Name: "MYSQL_ROOT_PASSWORD", Value: "root+1"}, {Name: "MYSQL_DATABASE", Value: "registry"}},
+					Command: []string{"/entrypoint.sh"},
+					Args:    []string{"mysqld"},
+				},
+			},
+		},
+	}
 }
